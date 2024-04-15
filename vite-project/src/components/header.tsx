64c124de6b2d7d774ce9c3ef721c92e9704faf47
alancastrom5 <79289644+alancastrom5@users.noreply.github.com>
2024-04-14 22:21:47 -0300

    mexendo um pouco

---
+++ vite-project/src/components/header.tsx
@@ -0,0 +1,16 @@
+
+import './header.css';
+
+interface HeaderProps{
+  title?: string;
+}
+
+export function Header({ title = "Curso React + TypeScript" }: HeaderProps){
+  return(
+    <header className="header">
+      <h1 className="title">{title}</h1>
+
+      <hr/>
+    </header>
+  )
+}
